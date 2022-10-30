1b273b680229e394a5a7ea45f5ad7008bfd6bb3d
daryaanto <dariaantonia.sandulescu@gmail.com>
2022-10-30 10:38:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Logo from './components/Logo';
+import Text from './components/tst';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo/>
+        <Text/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/tst.js
@@ -0,0 +1,4 @@
+function text(){
+    return <h1>Test</h1>
+}
+export default text;
