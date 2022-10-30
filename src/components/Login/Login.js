0c0b45d88d9d1a9d7ffbb24534bb942d7b333f67
Aida is Online <dellfin4@gmail.com>
2022-10-30 11:27:04 +0200

    Login page ready

---
+++ src/components/Login/Login.js
@@ -0,0 +1,23 @@
+import './Login.css';
+function Login() {
+    return(
+        <div class="Flex">
+            <div class="login-box">
+            <h1>Login</h1>
+            <div class="textbox">
+                <i class="fas fa-user"></i>
+                <input type="text" placeholder="Username"/>
+            </div>
+
+            <div class="textbox">
+                <i class="fas fa-lock"></i>
+                <input type="password" placeholder="Password"/>
+            </div>
+
+            <input type="button" class="btn" value="Sign in"/>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
